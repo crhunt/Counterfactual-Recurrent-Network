95a1258e269e41fcd63668e22e3fa36a51163bc1
crhunt <cassihunt@gmail.com>
2024-03-26 13:42:53 -0500

    CRN_models

--- CRN_models.py
+++ CRN_models.py
@@ -2,17 +2,16 @@
 import os
 import logging
 
+# This is trivial for compat but will be used for supporting a TF 2 / Keras 3 version of CRN.
+
 if 'CRN_VERSION' not in os.environ:
     logging.info("No env variable CRN_VERSION found. Defaulting to v1.")
-    from CRN_model import CRN_Model
     os.environ['CRN_VERSION'] = 'v1'
+
+if "compat" == os.environ['CRN_VERSION']:
+    from CRN_model import CRN_Model
 else:
-    if "v1" == os.environ['CRN_VERSION']:
-        from CRN_model import CRN_Model
-    elif "v2" == os.environ['CRN_VERSION']:
-        from CRN_model_v2 import CRN_Model
-    elif "compat" == os.environ['CRN_VERSION']:
-        from CRN_model import CRN_Model
-    else:
+    if "v1" != os.environ['CRN_VERSION']:
         logging.info("CRN_VERSION not valid. Defaulting to v1.")
-        from CRN_model import CRN_Model
+        os.environ['CRN_VERSION'] = 'v1'
+    from CRN_model import CRN_Model
